4a80ee2713450db21091481736415a66bc0d80fe
Greg Crutchlow <gregcrutchlow@gmail.com>
2024-09-01 10:01:11 -0400

    Initial commit

---
+++ src/scripts.js
@@ -0,0 +1,21 @@
+// function changeBG(bgColor, textColor) {
+// 	const body = document.querySelector('body');
+// 	(body.style.backgroundColor = bgColor), (body.style.color = textColor);
+// }
+
+// changeBG('#505050', '#fff');
+
+let favColor = document.getElementById('favcolor');
+function logColor() {
+	event.preventDefault();
+	const p = document.getElementById('color-output');
+	p.innerText = favColor.value;
+	return favColor.value;
+}
+
+const bgBtn = document.getElementById('change-bg');
+
+function changeBg() {
+	const body = document.querySelector('body');
+	body.style.backgroundColor = favColor.value;
+}
